1cdd40eab901dfcd37737a8b95b2ea290605c645
Ramona M <r1mari@yahoo.com>
2018-07-12 13:35:04 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -7,7 +7,7 @@ def get_first_name_of_season_winner(data, season)
     #string manipulation
       return contestant["name"].split(" ")[0]
       end
-end 
+  end 
 end
 
 def get_contestant_name(data, occupation)
